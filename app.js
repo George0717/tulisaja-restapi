1f4ecdf219aa9ca64e5086acef88fe156ebb2cdb
George0717 <raden7george@gmail.com>
2023-05-24 11:22:05 +0700

    update

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv/config')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+
+app.use(bodyParser.urlEncoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+app.use(cors())
+
+// import Route
+const postRoutes = require('./routes/post')
+app.use('/post',  postRoutes)
+// mongoose.connect(process.env.DB_CONNECTION, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+let db = mongoose.connection
+// Handle Error
+db.on('error', console.error.bind(console, 'Error Establishing a Database'))
+// Handle Succes
+db.once('open', () => {
+    console.log("Database is connected")
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.get('/', (req, res) => {
+    res.send('Hello World!3')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
 })
